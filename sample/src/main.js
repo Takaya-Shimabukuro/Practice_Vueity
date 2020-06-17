16681c69158e1068b95a639c56faf6f765c62b0e
Takaya-Shimabukuro <takayapublic@gmail.com>
2020-06-17 11:22:55 +0900

    ボタンの配置

--- sample/src/main.js
+++ sample/src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
@@ -8,3 +9,20 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+=======
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+function showSum() {
+  var num1 = 1;
+  var num2 = 2;
+  var sum = num1 + num2;
+  alert(sum);
+}
+>>>>>>> Stashed changes
